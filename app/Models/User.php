b9f002e666c430e38a437ce89369613b035fb984
Francis Beltre <francis.beltre.t@gmail.com>
2025-07-24 20:29:52 -0400

    feat: IA now interacts with DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function connections()
+    {
+        return $this->hasMany(Connection::class);
+    }
 }
